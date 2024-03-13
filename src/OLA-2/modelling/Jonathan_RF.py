66f36a259cf35886b2ff298087d41a6644f07e9f
Jonathanbraad <jonathanbraad123@gmail.com>
2024-03-13 10:32:34 +0100

    hafla

--- src/OLA-2/modelling/Jonathan_RF.py
+++ src/OLA-2/modelling/Jonathan_RF.py
@@ -6,9 +6,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, accuracy_score, roc_auc_score
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix
 
-df = pd.read_pickle('../data/cleaned.pkl')
-
+#df = pd.read_pickle('../data/cleaned.pkl')
+df = pd.read_pickle('../data/data_processed.pkl')
 
 # Splitting the DataFrame
 X = df.drop('HeartDisease', axis=1)  # Replace 'HeartDisease' with the actual target column name if different
@@ -24,12 +25,23 @@ rf_model.fit(X_train, y_train)
 
 # Make predictions on the test set
 y_pred = rf_model.predict(X_test)
-y_pred_proba = rf_model.predict_proba(X_test)[:, 1]  # For ROC-AUC calculation
 
-# Evaluate model - shows accuracy, ROC-AUC score, precision, recall, f1-score
+y_pred_proba = rf_model.predict_proba(X_test)[:, 1]  # For ROC-AUC calculation - 
+# - returns probability etimates for being 1
+
+cm = confusion_matrix(y_test, y_pred)
+
+# Evaluate model - shows accuracy, ROC-AUC score ( true positives against false positives), precision, recall, f1-score
 accuracy = accuracy_score(y_test, y_pred)
 roc_auc = roc_auc_score(y_test, y_pred_proba)
 print(f'Accuracy: {accuracy}')
 print(f'ROC-AUC: {roc_auc}')
 print(classification_report(y_test, y_pred))
+print("confusion matrix")
 
+# Print the confusion matrix with labels
+tn, fp, fn, tp = cm.ravel()
+print(f'True Negatives (TN): {tn}')
+print(f'False Positives (FP): {fp}')
+print(f'False Negatives (FN): {fn}')
+print(f'True Positives (TP): {tp}')
